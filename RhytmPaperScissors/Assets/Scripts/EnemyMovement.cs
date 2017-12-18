3dc0bdfa457d117bf9c48bffbab95692a83453bb
UTB\Ohlsab <eyber4@gmail.com>
2017-12-18 11:22:38 +0100

    Level grids, Lights and graphic

---
+++ RhytmPaperScissors/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private float playerDis;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //If player is close
+		if(true)
+        {
+            //If player is at attack range
+            if(true)
+            {
+
+            }
+        }
+	}
+
+    public void Distance(float dis)
+    {
+
+    }
+
+}
